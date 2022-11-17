31e8e93dd428e575db60f5182c5b54cfe61a7429
sam kim <ksmo9191@hanmail.net>
2022-11-17 20:12:16 +0900

    Modify add_window.py
    - Add Add_Window_Horizon_Weekly() function
    - Modify test code

--- lib/add_window.py
+++ lib/add_window.py
@@ -26,12 +26,40 @@ def Add_Window_Horizon(data, window=3, horizon=1, single=False):
     Y = np.array(Y)
     return X, Y
 
+
+def Add_Window_Horizon_Weekly(data, weeks_in=3, weeks_out=1, day_size=24):
+    '''
+    Generate Window Dataset Array by Week from Hourly/Daily Data
+    :param data: shape [B, ...]
+    :param weeks_in: number of weeks in the input sequence
+    :param weeks_out: number of weeks in the output sequence
+    :param day_size: number of data each day - when data is hourly data, day_size=24
+    :return: X is [iter_num, window_in, ...], Y is [iter_num, window_out, ...]
+    '''
+    X = []  # input sequences
+    Y = []  # output sequences
+    iter_num = len(data) - day_size*7*(weeks_in + weeks_out -1) - day_size + 1
+    for index in range(iter_num):
+        X.append(np.concatenate([
+            data[index+day_size*i:index+day_size*(i+1)] for i in range(0, 7*weeks_in, 7)
+        ]))
+        index = index+day_size*7*weeks_in
+        Y.append(np.concatenate([
+            data[index+day_size*i:index+day_size*(i+1)] for i in range(0, 7*weeks_out, 7)
+        ]))
+    X = np.array(X)
+    Y = np.array(Y)
+    return X, Y
+
+
 if __name__ == '__main__':
-    from data.load_raw_data import Load_Sydney_Demand_Data
-    path = '../data/1h_data_new3.csv'
-    data = Load_Sydney_Demand_Data(path)
-    print(data.shape)
-    X, Y = Add_Window_Horizon(data, horizon=2)
-    print(X.shape, Y.shape)
-
-
+    from load_dataset import load_st_dataset
+    import time
+    st_time = time.time()
+    
+    dataset = 'LIVPOP'
+    data = load_st_dataset(dataset)    
+    print('>>> before shape :', data.shape)
+    X, Y = Add_Window_Horizon_Weekly(data)
+    print('>>> after shape :', X.shape, Y.shape)
+    print(f'>>> Time Elapsed : {time.time()-st_time:.1f} s.')
